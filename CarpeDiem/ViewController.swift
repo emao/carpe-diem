b5cbfcd65ec9405b8db79a96d34c07ac61d5692d
Eric Mao <maoeric77@gmail.com>
2017-01-21 17:53:22 -0800

    Initial Commit

---
+++ CarpeDiem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarpeDiem
+//
+//  Created by Eric Mao on 1/21/17.
+//  Copyright © 2017 Eric Mao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
